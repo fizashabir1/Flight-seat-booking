28e546db8e8884fcd07179f61a99178bb5025c9e
Fiza Shabir <l1f24bsse0202@ucp.edu.pk>
2026-05-22 23:06:15 +0500

    initial project setup and github integration setup intellij project with github Created basic structure of Flight Seat Booking

---
+++ src/Flightseatbooking.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+class Passenger {
+
+    int id;
+    String name;
+    int seatNo;
+
+    Passenger(int id, String name, int seatNo) {
+        this.id = id;
+        this.name = name;
+        this.seatNo = seatNo;
+    }
+}
+public class Flightseatbooking {
+
+    public static void main(String[] args) {
+
+        System.out.println("Flight Seat Booking System Started");
+    }
+}
